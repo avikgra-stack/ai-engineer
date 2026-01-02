ec3081d33a49b7663bbd02f50f64bb4a271ef28c
avikgra-stack <avikgra@gmail.com>
2026-01-02 17:50:56 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ai-engineer/', // Имя вашего репозитория
+  base: '/ai-engineer/',
   build: {
-    outDir: 'dist',
-  }
+    rollupOptions: {
+      // Сообщаем сборщику, что эти модули внешние и их не надо искать в проекте
+      external: [
+        'react',
+        'react-dom',
+        'react-dom/client',
+        'lucide-react',
+        '@google/genai'
+      ],
+    },
+  },
 })
