8c40b4337264a43647c756224fa37c93ec15573f
avikgra-stack <avikgra@gmail.com>
2026-01-02 15:04:02 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
+      base: '/ai-engineer/', // Обязательно: имя репозитория в слэшах
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
